0ca975d7a6c46dc7c6bd7b8fe9d735f1b7734932
Davide Donadio <ddonadio@ucdavis.edu>
2022-11-18 11:12:38 -0800

    Update compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -2,6 +2,7 @@ FC=mpif90
 FFLAGS=" -fopenmp -m64"
 LAPACK="-lmkl_lapack95_lp64 -lmkl_gf_lp64 -lmkl_core -lmkl_gnu_thread"
 
-${FC} ${FFLAGS} -o relaxmpi.x relaxmpi.f90 ${LAPACK}
-${FC} ${FFLAGS} -o RdiagDynMat.x diagDynMat.f90 ${LAPACK}
-${FC} ${FFLAGS} -o ~/bin/QHGKd.x QHGKd.f90 ${LAPACK}
+mkdir -p ../bin
+${FC} ${FFLAGS} -o ../bin/relaxmpi.x relaxmpi.f90 ${LAPACK}
+${FC} ${FFLAGS} -o ../bin/RdiagDynMat.x diagDynMat.f90 ${LAPACK}
+${FC} ${FFLAGS} -o ../bin/QHGKd.x QHGKd.f90 ${LAPACK}
